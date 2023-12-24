9d169557eacb996ab15b06ec20c6796f868687b9
unknown <hamadaemamemam1@gmail.com>
2023-12-24 21:22:48 +0200

    some modify on header section

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
 // Show Menu Bar
-const mune = document.querySelector('.fa-solid');
+const mune = document.querySelector('.menu');
 console.log(mune)
+const ul = document.querySelector('nav ul');
 mune.addEventListener("click", function () {
-    const ul = document.querySelector('nav ul');
-    const lis = document.querySelectorAll('nav ul li')
-    const lia = document.querySelectorAll('nav ul li a')
-    ul.style.cssText = `display:flex; flex-direction : column;
-    position:absolute;top:100%; left:0; width:100%`;
-    for (item of lis) {
-        item.style.cssText = `padding: 0 10px 15px !important;`
-        console.log(item);
-    }
-    for (item of lia) {
-        item.style.cssText = `padding: 0 10px 15px !important;`
-    }
-
+    ul.classList.toggle('small');
 });
